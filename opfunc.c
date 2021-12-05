c25700a5bccbd03d5d1ff37a35e4b3a4f25849ea
IyasuH <eyasuhailegbr@gmail.com>
2021-12-05 12:58:39 -0800

    functions rearranged

--- opfunc.c
+++ opfunc.c
@@ -120,39 +120,3 @@ swap = (*stack)->n;
 (*stack)->n = (*stack)->next->n;
 (*stack)->next->n = swap;
 }
-
-/**
- * add - adds the top two elements of the stack
- * @stack: doubly linked list
- * @line_number: the number of line
- *
- * Return: nothing
- */
-
-void add(stack_t **stack, unsigned int line_number)
-{
-int temp;
-
-if (*stack == NULL || (*stack)->next == NULL)
-{
-printf("L%u: can't add, stack too short\n", line_number);
-exit(EXIT_FAILURE);
-}
-temp = (*stack)->n;
-pop(stack, line_number);
-(*stack)->n += temp;
-}
-
-/**
- * nop - doesn't do anything
- * @stack: doubly linked list
- * @line_number: the number of line
- *
- * Return: nothing
- */
-
-void nop(stack_t **stack, unsigned int line_number)
-{
-(void)stack;
-(void)line_number;
-}
